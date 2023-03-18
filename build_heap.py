f04b62ad32f977447100353fd8a06ee2f6aca75e
evastakava <125592045+evastakava@users.noreply.github.com>
2023-03-18 13:10:34 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -2,52 +2,68 @@
 
 def build_heap(data):
     swaps = []
+    # TODO: Creat heap and heap sort
+    # try to achieve  O(n) and not O(n2)
     n = len(data)
 
     for i in range(n // 2, -1, -1):
-        sift_down(i, data, swaps)
-    
+        j = 1
+        while True:
+            left_child = 2*j+1
+            right_child = 2*j+2
+            if left_child < n and data[left_child] > data [j]:
+                j = left_child
+            if right_child < n and data[right_child] > data[j]:
+                j = right_child
+            if i != j:
+                data[i], data[j] = data[j], data[i]
+                swaps.append((i, j))
+                i = j
+            else:
+                break
+
     return swaps
 
 
-def sift_down(i, data, swaps):
-
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2
-    min_index = i
-    if left_child < len(data) and data[left_child] < data[min_index]:
-        min_index = left_child
-    if right_child < len(data) and data[right_child] < data[min_index]:
-        min_index = right_child
-
-    if i != min_index:
-        swaps.append((i, min_index))
-        data[i], data[min_index] = data[min_index], data[i]
-        sift_down(min_index, data, swaps)
-
-
 def main():
-    input_type = input().strip()
+    
+    # TODO : add input and corresponding checks
+    # add another input for I or F 
+    # first two tests are from keyboard, third test is from a file
+    input_type = input("Enter I for keyboard input or F for file input: ")
 
+    # input from keyboard
     if input_type == 'I':
         n = int(input())
         data = list(map(int, input().split()))
-        assert len(data) == n
-        assert len(data) == len(set(data))
-        swaps = build_heap(data)
-
     elif input_type == 'F':
-        with open('input.txt', 'r') as f:
-            n = int(f.readline().strip())
-            data = list(map(int, f.realine().strip().split()))
-            assert len(data) == n
-            assert len(data) == len(set(data))
-            swaps = build_heap(data)
-            with open('output.txt', 'w') as out_f:
-                out_f.write(str(len(swaps)) + '\n')
-                for i, j in swaps:
-                    out_f.write(str(i) + ' ' + str(j) + '\n')
-    
-    
+        file_name = input("Enter file name: ")
+        with open(file_name, "r") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+
+    else:
+        print("Invalid input type")
+        return
+
+    # checks if lenght of data is the same as the said lenght
+    assert len(data) == n
+    assert len(data) == len(set(data))
+
+    # calls function to assess the data 
+    # and give back all swaps
+    swaps = build_heap(data)
+
+    # TODO: output how many swaps were made, 
+    print(len(swaps))
+    # this number should be less than 4n (less than 4*len(data))
+    assert len(swaps) <= 4*n, "Number of swaps exceeds 4n"
+
+
+    # output all swaps
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+
 if __name__ == "__main__":
     main()
